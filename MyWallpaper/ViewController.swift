e5b2739b93055920b08fd5f57dd221e44a2c7052
pham vinh dat <phamvinhdat1998@gmail.com>
2018-11-21 13:05:32 +0700

    continue

--- MyWallpaper/ViewController.swift
+++ MyWallpaper/ViewController.swift
@@ -7,18 +7,23 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
 
-    let strURL = "https://pixabay.com/api/?key=10738309-2ce5419b996ea7e48b49dc189"
+    let strUrlRoot = "https://pixabay.com/api/?key=10738309-2ce5419b996ea7e48b49dc189"
     var webReturn: WebReturn?
     var searchBar:UISearchBar? = nil
+    var isContinue = true
+    var currentPage = 1
+    var maxPage: Int? = nil
     @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         myTableView.dataSource = self
         myTableView.delegate = self
-        loadData()
+        loadData(search: nil, page: 2)
+        createSearchBar()
     }
     
     struct Pixabay: Decodable{
@@ -37,14 +42,19 @@ class ViewController: UIViewController {
     
     struct WebReturn: Decodable{
         let totalHits: Int
-        let hits: [Pixabay]
+        var hits: [Pixabay]
     }
     
-    fileprivate func loadData(){
-        guard let url = URL(string: strURL) else{return}
+    fileprivate func loadData(search: String?, page: Int = 1){
+        guard maxPage != nil &&  maxPage! > page else {return}
+        var strUrl = "\(self.strUrlRoot)&page=\(page)"
+        let strSearch = search != nil ? "&q=\(search!)" : ""
+        strUrl.append(strSearch)
+        guard let url = URL(string: strUrl) else{return}
+        
         URLSession.shared.dataTask(with: url) { (data, response, err) in
-            
             DispatchQueue.main.async {
+                
                 if let err = err{
                     print("Failed to get data from URL: ", err)
                     return
@@ -54,36 +64,52 @@ class ViewController: UIViewController {
                 
                 do{
                     let decoder = JSONDecoder()
-                    self.webReturn = try decoder.decode(WebReturn.self, from: data)
+                    let temp = try decoder.decode(WebReturn.self, from: data)
+                    if self.isContinue{
+                        self.webReturn?.hits.insert(contentsOf: temp, at: 0)
+                    }
                 }catch let jsonErr{
                     print("Failed to decode: ", jsonErr)
                 }
+                
                 self.myTableView.reloadData()
             }
-            }.resume()
+        }.resume()
+        
         self.myTableView.reloadData()
     }
-
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource{
+extension ViewController: UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return webReturn != nil ? webReturn!.hits.count : 10
+        return webReturn != nil ? webReturn!.hits.count : 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath) as! MyTableViewCell
         if let temp = webReturn{
-            cell.textLabel?.text = temp.hits[indexPath.row].tags
-            //cell.imgShow.image
+            cell.imgShow.sd_setImage(with: temp.hits[indexPath.row].imgURL, placeholderImage: #imageLiteral(resourceName: "400x200"))
         }else{
-            cell.textLabel?.text = "dmm: \(indexPath.row)"
             cell.imgShow.image = #imageLiteral(resourceName: "400x200")
         }
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if let temp = webReturn{
+            let width = self.view.frame.width
+            let raito = CGFloat(temp.hits[indexPath.row].width) / width
+            return CGFloat(temp.hits[indexPath.row].height) / raito
+        }else{
+            return 200
+        }
+    }
     
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView.contentOffset.y < 0{
+            print("adsad")
+        }
+    }
 }
 
 extension ViewController: UISearchBarDelegate{
---
+++ Pods/Target Support Files/Pods-MyWallpaper/Pods-MyWallpaper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyWallpaper : NSObject
+@end
+@implementation PodsDummy_Pods_MyWallpaper
+@end
