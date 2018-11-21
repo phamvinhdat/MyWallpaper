247aa7126aec86d7e8da088f7acb1b0953aa5bd6
pham vinh dat <phamvinhdat1998@gmail.com>
2018-11-22 02:12:09 +0700

    ok

--- MyWallpaper/ImageViewController.swift
+++ MyWallpaper/ImageViewController.swift
@@ -11,17 +11,57 @@ import UIKit
 class ImageViewController: UIViewController {
 
     var myImg = #imageLiteral(resourceName: "400x200")
-    var heightView:CGFloat = 200
+    var isFull = false
     
     @IBOutlet weak var viewImg: UIView!
     @IBOutlet weak var imgSow: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let width = self.view.frame.width
-        viewImg.frame = CGRect(x: 0, y: 0, width: 10, height: 10)
+        
         imgSow.image = myImg
     }
     
-
+    @IBAction func btnSaveImg(_ sender: Any) {
+        UIImageWriteToSavedPhotosAlbum(myImg, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    @IBAction func btnFullScreen(_ sender: Any) {
+        let btn = sender as! UIButton
+        if !isFull{
+            self.imgSow.contentMode = .center
+            btn.setImage(#imageLiteral(resourceName: "icons8-normal_screen"), for: .normal)
+        }else{
+            self.imgSow.contentMode = .scaleAspectFit
+            btn.setImage(#imageLiteral(resourceName: "icons8-full_screen"), for: .normal)
+        }
+        isFull = !isFull
+    }
+    
+    @IBAction func btnShare(_ sender: Any) {
+        //tham khao tren mang
+        // set up activity view controller
+        let imageToShare = [ imgSow! ]
+        let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+        
+        // exclude some activity types from the list (optional)
+        activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook , ]
+        
+        // present the view controller
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
 }
